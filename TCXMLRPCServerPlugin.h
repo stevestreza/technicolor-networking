0c833d9be27ca42285938280aa2a1384bb3bc5a9
Steve Streza <stevestreza@gmail.com>
2008-11-22 23:08:09 -0500

    Initial commit

---
+++ TCXMLRPCServerPlugin.h
@@ -0,0 +1,24 @@
+//
+//  TCXMLRPCServerPlugin.h
+//  Technicolor
+//
+//  Created by Steve Streza on 11/17/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "Technicolor.h"
+#import "TCOrganizationPlugin.h"
+#import "XMLRPCServer.h"
+
+#import "TCXMLRPCTVEpisodeHandler.h"
+
+#define kTCHTTPServerPluginUUID @"EDD4962D-4BCC-4CF5-A7B0-35FC22E85B09"
+
+@interface TCXMLRPCServerPlugin : TCOrganizationPlugin {
+	id server;
+	
+	NSArray *handlers;
+}
+
+@end
